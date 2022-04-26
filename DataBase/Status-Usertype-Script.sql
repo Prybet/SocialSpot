354332c95af0adfadd6824dffdea53fbaa0e2f9a
Prybet <mrprybet@outlook.com>
2022-04-26 00:01:11 -0400

    DBModel and Status-Usertype Script
    Modelo de la bd y insert de estados y tipos de usuario

---
+++ DataBase/Status-Usertype-Script.sql
@@ -0,0 +1,23 @@
+INSERT INTO status VALUES(null, "Active");
+INSERT INTO status VALUES(null, "On-Line");
+INSERT INTO status VALUES(null, "Away");
+INSERT INTO status VALUES(null, "Disconnected");
+INSERT INTO status VALUES(null, "Hidden");
+INSERT INTO status VALUES(null, "Removed");
+INSERT INTO status VALUES(null, "Edited");
+INSERT INTO status VALUES(null, "Visible");
+INSERT INTO status VALUES(null, "Pending");
+INSERT INTO status VALUES(null, "Closed");
+INSERT INTO status VALUES(null, "Resolved");
+INSERT INTO status VALUES(null, "Given");
+INSERT INTO status VALUES(null, "Banned");
+INSERT INTO status VALUES(null, "Banned");
+
+
+
+INSERT INTO usertype VALUES(null, "User",1);
+INSERT INTO usertype VALUES(null, "Guess",1);
+INSERT INTO usertype VALUES(null, "Client",1);
+INSERT INTO usertype VALUES(null, "Modder",1);
+INSERT INTO usertype VALUES(null, "Admin",1);
+
