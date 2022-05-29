6ac749fd74c543bde201f7c91e763414739c8ed9
nachosk1 <pedroale328@gmail.com>
2022-05-28 21:10:58 -0400

    change nav user
     el diseño del usuario del nav esta completo

--- js/nav.js
+++ js/nav.js
@@ -12,17 +12,26 @@ var modalU = document.querySelector(".contain_option");
 var decoU = document.querySelector(".decorate_user");
 
 
-btnUser.addEventListener("click", function(){
-    modalU.style.visibility = "visible";
-    decoU.style.visibility = "visible";
-});
 
 
 $(document).on("click",function(e) {
-    var btnU = $("#btn_user");  
+    var btnU = $("#btn_user"); 
+    var imgU = $(".img-user"); 
+    var lblU = $(".lbluser"); 
+
     var container = $(".contain_option");            
-    if (container.is(e.target)) { 
+    if (!container.is(e.target) && container.has(e.target).length === 0) { 
+        //si se disparo el btn se hace la condicion 
+        if(btnU.is(e.target) || imgU.is(e.target) || lblU.is(e.target)){
+            console.log("true");
+            modalU.style.visibility = "visible";
+            decoU.style.visibility = "visible";
+        }else{
+            modalU.style.visibility = "hidden";
+            decoU.style.visibility = "hidden";
+            console.log("else");
+        }
         
-        modalU.style.visibility = "hidden";
     }
 });
+
--- nav.php
+++ nav.php
@@ -34,37 +34,42 @@
 
         
         <div class="contain_user btn-show flex">
-            <div class="img_user flex" id="btn_user" for="checkbox_menu">
-                <img src="../img/perfil.png" class="size-img img-user pointer" />
-                <div class="lbluser pointer">Usuario</div>
-                <div class="decorate_user "></div>
-            </div>
+            <button class="btn_user-nav">
+                <div class="img_user flex" id="btn_user" for="checkbox_menu">
+                    <img src="../img/perfil.png" class="size-img img-user pointer" />
+                    <div class="lbluser pointer">Usuario</div>
+                    <div class="decorate_user "></div>
+                </div>
+            </button>
+            
             
             <div class="contain_option " id="conteiner_option">
-                <div class="option flex">
+                <button class="option flex">
                     <div class="op flex" id="btn_userr">
                         <img src="../img/user.png" class="separation size-img pointer">
                         <label class="pointer">Ver Perfil</label>
                     </div>
-                </div>
-                <label class="content-input option flex">
-                    <div class="op flex">
-                        <input type="checkbox">Conectado<i></i>
-                    </div>
-                </label>
-                <div class="option flex">
+                </button>
+                <button class="option flex">
+                    <label class="content-input">
+                        <div class="op flex">
+                            <input type="checkbox">Conectado<i></i>
+                        </div>
+                    </label>
+                </button>
+                <button class="option flex">
                     <div class="xx op flex">
                         <img src="../img/configuration.png" class="separation size-img pointer">
                         <label class="pointer">Editar Perfil</label>
                     </div>
-                </div>
+                </button>
                 <div class="line"></div>
-                <div class="option flex">
+                <button class="option flex">
                     <div class="op flex">
                         <img src="../img/exit.png" class="separation size-img pointer">
                         <label class="lbl_c pointer">Cerrar Sesion</label>
                     </div>
-                </div>
+                </button>
             </div>
         </div>
     </div>
