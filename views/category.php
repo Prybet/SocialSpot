d242ba6b08b9110a49129092abd0e0a43e1a7952
nachosk1 <pedroale328@gmail.com>
2022-06-22 18:56:37 -0400

    change
    listo like, con susvalidaciones si lo selecciona un usuario invitado o no, ademas se arreglo la sentencia del la clase de post del profile.
    Se añadio las vistas de profile publico y categoria la cual aun esta en proceso.

---
+++ views/category.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+SocialSpot 2022
+Made by: 
+ jasmet_generico1
+ soulbroken
+ Prybet
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
